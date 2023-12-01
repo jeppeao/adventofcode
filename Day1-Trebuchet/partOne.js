03dcd33ebdea7e93cdea98ab53ca25e6507468d8
jeppeao <jeppeao@gmail.com>
2023-12-01 17:05:49 +0000

    solved part one

--- Day1-Trebuchet/partOne.js
+++ Day1-Trebuchet/partOne.js
@@ -25,3 +25,26 @@ In this example, the calibration values of these four lines are 12, 38, 15, and 
 Consider your entire calibration document. What is the sum of all of the calibration values?
 */
 
+const { readFile } = require('fs/promises');
+
+const testString = `1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`
+
+const firstDigitRe = /\d/;
+const lastDigitRe = /(\d)(?!.*\d)/;
+
+const findNumber = (str) => {
+  return parseInt(str.match(firstDigitRe) + str.match(lastDigitRe));
+}
+
+const sumCalibrationValues = (text) => {
+  return text.split("\n").reduce((acc, line) =>  acc + findNumber(line), 0)
+}
+
+readFile('./puzzleInput.txt', 'utf-8')
+  .then(file => sumCalibrationValues(file))
+  .then(sum => console.log(sum))
+
+
