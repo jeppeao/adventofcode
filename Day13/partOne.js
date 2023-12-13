f3a83bdf203993dd49f2153d9dcaae520cba8ead
jeppeao <jeppeao@gmail.com>
2023-12-13 11:13:49 +0000

    day 13 part one

--- Day13/partOne.js
+++ Day13/partOne.js
@@ -3,26 +3,83 @@
 import { readFile } from 'fs/promises'
 
 const testData = 
-`...#......
-.......#..
-#.........
-..........
-......#...
-.#........
-.........#
-..........
-.......#..
-#...#.....`
+`#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#
+`
 
 const parse = (data) => {
-  const parsed = '';
-  return parsed;
-}
-const solve = (data) => {
-  const parsed = parse(data);
-  return parsed;
+  const patterns = data.trim()
+    .split('\n\n')
+    .map(p => p.split('\n').map(p => p.split('')));
+  return patterns;
 }
 
-readFile('./Day12/puzzleInput.txt', 'utf-8')
-  .then(file => solve(testData))
+const getRowsAndColumns = (pattern) => {
+  const cols = pattern[0].map(
+    (_, i) => pattern.map(line => line[i])
+  );
+  return [pattern, cols];
+}
+
+const areSame = (v1, v2) => {
+  if (v1.length !== v2.length) return false;
+  for (let i=0; i<v1.length; i++) {
+    if (v1[i] !== v2[i]) return false
+  }
+  return true;
+}
+
+const findReflection = (lines) => {
+  for (let i=1; i<lines.length; i++) {
+    let left = i-1;
+    let right = i;
+    let matches = true;
+    while(left >= 0 && right < lines.length) {
+      if (!areSame(lines[left], lines[right])) {
+        matches = false;
+      }
+      left--;
+      right++;
+    }
+    if (matches) {
+      return i;
+    }
+  }
+  return false
+}
+
+const scorePattern = (pattern) => {
+  const [rows, cols] = getRowsAndColumns(pattern);
+  let mirrorHorizontal = findReflection(rows);
+  let mirrorVertical = findReflection(cols);
+
+  if (mirrorVertical) {
+    return mirrorVertical;
+  }
+
+  return mirrorHorizontal * 100;
+}
+
+const solve = (data) => {
+  const parsed = parse(data);
+  const scores = parsed.map(scorePattern)
+  
+  return scores.reduce((acc, val) => acc+val, 0);
+}
+
+readFile('./Day13/puzzleInput.txt', 'utf-8')
+  .then(file => solve(file))
   .then(res => console.log(res))
---
+++ Day14/partOne.js
@@ -0,0 +1,28 @@
+// https://adventofcode.com/2023/day/12#part1
+
+import { readFile } from 'fs/promises'
+
+const testData = 
+`...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....`
+
+const parse = (data) => {
+  const parsed = '';
+  return parsed;
+}
+const solve = (data) => {
+  const parsed = parse(data);
+  return parsed;
+}
+
+readFile('./Day12/puzzleInput.txt', 'utf-8')
+  .then(file => solve(testData))
+  .then(res => console.log(res))
