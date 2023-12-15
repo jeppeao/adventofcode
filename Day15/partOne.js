bc292572839c0fa18a100f4fd356b112566d1911
jeppeao <jeppeao@gmail.com>
2023-12-15 09:29:22 +0000

    Day15 part One

--- Day15/partOne.js
+++ Day15/partOne.js
@@ -1,28 +1,39 @@
-// https://adventofcode.com/2023/day/15#part2
+// https://adventofcode.com/2023/day/15#part1
 
 import { readFile } from 'fs/promises'
 
 const testData = 
-`...#......
-.......#..
-#.........
-..........
-......#...
-.#........
-.........#
-..........
-.......#..
-#...#.....`
+`rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
+`
 
 const parse = (data) => {
-  const parsed = '';
-  return parsed;
-}
-const solve = (data) => {
-  const parsed = parse(data);
+  const parsed = data.replace(/[\r\n]/gm, '').trim().split(',');
   return parsed;
 }
 
-readFile('./Day12/puzzleInput.txt', 'utf-8')
-  .then(file => solve(testData))
+const hash = (input) => {
+  let curVal = 0;
+  for (let i=0; i<input.length; i++) {
+    let ASCII = input.charCodeAt(i);
+    curVal += ASCII;
+    curVal *= 17;
+    curVal = curVal % 256;
+  }
+  return curVal;
+}
+
+const solve = (data) => {
+  const parsed = parse(data);
+  let total = 0;
+  for (let str of parsed) {
+    let hashed = hash(str);
+    total += hashed;
+  }
+  return total;
+}
+
+readFile('./Day15/puzzleInput.txt', 'utf-8')
+  .then(file => solve(file))
   .then(res => console.log(res))
+
+  
