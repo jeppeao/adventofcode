11e6dc82e087d0efbe1dd4020cd813c4c93701d4
jeppeao <jeppeao@gmail.com>
2023-12-23 23:07:44 +0100

    day23 both parts

--- Day23/partOne.js
+++ Day23/partOne.js
@@ -1,28 +1,160 @@
-// https://adventofcode.com/2023/day/15#part2
+// https://adventofcode.com/2023/day/23#part1
 
 import { readFile } from 'fs/promises'
 
 const testData = 
-`...#......
-.......#..
-#.........
-..........
-......#...
-.#........
-.........#
-..........
-.......#..
-#...#.....`
+`#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.>...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#####v#.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#
+`
 
 const parse = (data) => {
-  const parsed = '';
-  return parsed;
-}
-const solve = (data) => {
-  const parsed = parse(data);
-  return parsed;
+  const rows = data.trim()
+    .split('\n')
+    .map(r => r.split(''));
+  const items = rows.flat();
+  const nRows = rows.length;
+  const nCols = rows[0].length;
+  const startCol = rows[0].indexOf('.');
+  const endCol = rows[rows.length -1].indexOf('.');
+
+  return [items, startCol, endCol, nCols];
 }
 
-readFile('./Day12/puzzleInput.txt', 'utf-8')
-  .then(file => solve(testData))
+const validNextSteps = (pos, items, nCol) => {
+  if (items[pos] === '^') return [pos - nCol];
+  if (items[pos] === 'v') return [pos + nCol];
+  if (items[pos] === '>') return [pos + 1];
+  if (items[pos] === '<') return [pos - 1];
+
+  let valid = [];
+  if (pos - nCol >= 0 && items[pos-nCol] !== 'v') {
+    valid.push(pos - nCol);
+  }
+  if (pos + nCol < items.length && items[pos + nCol] !== '^') {
+    valid.push(pos + nCol)
+  }
+  if (sameRow(pos, pos-1, nCol) && items[pos-1] !== '>') {
+    valid.push(pos - 1);
+  }
+  if (sameRow(pos, pos+1, nCol) && items[pos+1] !== '<') {
+    valid.push(pos + 1);
+  }
+
+  return valid.filter(pos => items[pos] !== '#')
+}
+
+const sameRow = (p1, p2, cols) => {
+  return (Math.floor(p1 / cols) === Math.floor(p2 / cols));
+}
+
+const findConnections = (pos, items, nCol, end) => {
+  let q = [{pos, len: 0}];
+  let visited = new Set();
+  visited.add(pos);
+  let connections = {};
+
+  while (q.length > 0) {
+    let { pos, len } = q.pop();
+    len++;
+    let next = validNextSteps(pos, items, nCol);
+    next = next.filter(n => !visited.has(n))
+    if (next.length > 1) {
+      for (let n of next) {
+        if (!connections[n]) { 
+          connections[n] = len;
+        }
+        else {
+          if (connections[n] < len) {
+            connections[n] = len;
+          }
+        }
+      }
+    }
+    if (next.includes(end)) {
+      if (!connections[end]) { 
+        connections[end] = len;
+      }
+      else {
+        if (connections[end] < len) {
+          connections[end] = len;
+        }
+      }
+    }
+    else if (next.length === 1 && !visited.has(next[0])) {
+      visited.add(next[0]);
+      q.push({pos: next[0], len})
+    }
+  }
+  return connections;
+}
+
+const findAllConnected = (items, nCol, start, end) => {
+  let q = [start];
+  const connections = {};
+  const visited = [start];
+  while (q.length > 0) {
+    let cur = parseInt(q.pop());
+    let conns = findConnections(cur, items, nCol, end);
+    connections[cur] = conns;
+    for (let c of Object.keys(conns)) {
+      if (!visited.includes(c)) {
+        q.push(c);
+        visited.push(c);
+      }
+    }
+  }
+  return connections;
+}
+
+const findPaths = (conns, start, end) => {
+  const q = [{pos: start, len: 0}];
+  const paths = [];
+  while (q.length > 0) {
+
+    let { pos, len } = q.pop();
+    let next = conns[pos];
+    for (let n of Object.keys(next)) {
+      let extLen = len + next[n];
+      if (n === end) {
+        paths.push(extLen);
+      }
+      else {
+        q.push({pos: n, len: extLen})
+      }
+    }
+  }
+  return paths;
+}
+
+const solve = (data) => {
+  const [items, startCol, endCol, nCols] = parse(data);
+  let end = items.length - 2;
+  let conns = findAllConnected(items, nCols, 1, end);
+  let paths = findPaths(conns, 1, end.toString());
+  return paths.reduce((acc, val) => val > acc ? val : acc, paths[0]);
+}
+
+readFile('./Day23/puzzleInput.txt', 'utf-8')
+  .then(file => solve(file))
   .then(res => console.log(res))
--- Day23/partTwo.js
+++ Day23/partTwo.js
@@ -1,28 +1,154 @@
-// https://adventofcode.com/2023/day/15#part2
+// https://adventofcode.com/2023/day/23#part2
 
 import { readFile } from 'fs/promises'
 
 const testData = 
-`...#......
-.......#..
-#.........
-..........
-......#...
-.#........
-.........#
-..........
-.......#..
-#...#.....`
+`#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.>...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#####v#.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#
+`
 
 const parse = (data) => {
-  const parsed = '';
-  return parsed;
-}
-const solve = (data) => {
-  const parsed = parse(data);
-  return parsed;
+  const rows = data.trim()
+    .split('\n')
+    .map(r => r.split(''));
+  const items = rows.flat();
+  const nRows = rows.length;
+  const nCols = rows[0].length;
+  const startCol = rows[0].indexOf('.');
+  const endCol = rows[rows.length -1].indexOf('.');
+
+  return [items, startCol, endCol, nCols];
 }
 
-readFile('./Day12/puzzleInput.txt', 'utf-8')
-  .then(file => solve(testData))
+const validNextSteps = (pos, items, nCol) => {
+  let valid = [];
+  pos = parseInt(pos);
+  if (pos - nCol >= 0) {
+    valid.push(pos - nCol);
+  }
+  if (pos + nCol < items.length) {
+    valid.push(pos + nCol)
+  }
+  if (sameRow(pos, pos-1, nCol)) {
+    valid.push(pos - 1);
+  }
+  if (sameRow(pos, pos+1, nCol)) {
+    valid.push(pos + 1);
+  }
+
+  return valid.filter(pos => items[pos] !== '#').map(Number)
+}
+
+const sameRow = (p1, p2, cols) => {
+  return (Math.floor(p1 / cols) === Math.floor(p2 / cols));
+}
+
+const findConnections = (pos, items, nCol, end) => {
+  let connections = {};
+  let pathStarts = validNextSteps(pos, items, nCol);
+  for (let p of pathStarts) {
+    let visited = [pos, p];
+    let len = 1;
+    let next = [p]
+    while (next.length === 1) {
+      let cur = next.pop();
+      if (cur === end) {
+        connections[cur] = len;
+        break;
+      }
+      let following = validNextSteps(cur, items, nCol);
+      following = following.filter(v => !visited.includes(v));
+      visited = visited.concat(following);
+      if (following.length > 1) {
+        connections[cur] = len;
+        break;
+      }
+      next = next.concat(following);
+      len++
+    }
+  }
+  return connections;
+}
+
+const findAllConnected = (items, nCol, start, end) => {
+  let q = [start];
+  const connections = {};
+  const visited = [start];
+  while (q.length > 0) {
+    let cur = parseInt(q.pop());
+    let conns = findConnections(cur, items, nCol, end);
+    connections[cur] = conns;
+    for (let c of Object.keys(conns)) {
+      if (!visited.includes(c)) {
+        q.push(c);
+        visited.push(c);
+      }
+    }
+  }
+  return connections;
+}
+
+const findPaths = (conns, start, end) => {
+  const q = [[start]];
+  const paths = [];
+  while (q.length > 0) {
+    let cur = q.pop();
+    let last = cur[cur.length-1];
+    let next = conns[last];
+    if (last === end) {
+      paths.push(cur);
+    }
+    else {
+      for (let n of Object.keys(next)) {
+        let val = parseInt(n);
+        if (!cur.includes(val)) {
+          q.push([...cur, val])
+        }
+      }
+    }
+  }
+  return paths;
+}
+
+const pathToLength = (path, conn) => {
+  let len = 0;
+  for (let i=1; i<path.length; i++) {
+    len += conn[path[i-1]][path[i]]
+  }
+  return len;
+}
+
+const solve = (data) => {
+  const [items, startCol, endCol, nCols] = parse(data);
+  let end = items.length - 2;
+  let conns = findAllConnected(items, nCols, 1, end);
+  let paths = findPaths(conns, 1, end);
+  paths = paths.map(p => pathToLength(p, conns))
+
+  return paths.reduce((acc, val) => val > acc ? val : acc, paths[0]);
+}
+
+readFile('./Day23/puzzleInput.txt', 'utf-8')
+  .then(file => solve(file))
   .then(res => console.log(res))
